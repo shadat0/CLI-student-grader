8db6a78607b8109092560daa6fc5327e4651210d
Shahadat Hossain <31365868+shadat0@users.noreply.github.com>
2026-04-11 17:59:21 +0400

    created app menu and title

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  // settings
+  const appTitle = "Student Grader v1.0"
+  print("==== $appTitle ====");
+  final subjects = ["Math", "Science", "English", "History"];
+
+  // Main menu
+  print('1. Add Student');
+  print('2. Record Score');
+  print('3. Add Bonus Points');
+  print('4. Add Comment');
+  print('5. View All Students');
+  print('6. View Report Card');
+  print('7. Class Summary');
+  print('8. Exit');
+}
